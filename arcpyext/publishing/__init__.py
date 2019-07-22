d03ea07f499ec9d0792c9aa9c29f3a16fa9c2440
Chris Blanchfield <48502250+christopherblanchfield@users.noreply.github.com>
2019-07-22 10:26:44 +1000

    Hosted feature service support

--- arcpyext/publishing/__init__.py
+++ arcpyext/publishing/__init__.py
@@ -5,4 +5,5 @@ try:
 except:
     from ._publishing import (convert_pro_map_to_service_draft as convert_map_to_service_draft,
                               convert_pro_map_to_vector_tile_draft as convert_pro_map_to_vector_tile_draft,
+                              convert_pro_map_to_hosted_feature_draft as convert_pro_map_to_hosted_feature_draft,
                               convert_service_draft_to_staged_service)
